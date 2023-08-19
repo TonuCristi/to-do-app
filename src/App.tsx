d8ac1774eea0bd0c7a8a5b1ff4fa558dc7376a84
cristian <chrisdev2002@gmail.com>
2023-08-19 13:41:32 +0300

    removed another line

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import AddBtn from "./components/AddBtn";
 import Item from "./components/Item/Item";
 import Form from "./components/Form/Form";
 import { useState } from "react";
-import RecordBn from "./components/RecordBn";
 
 const Container = styled.div`
   background-color: #282c34;
